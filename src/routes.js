7decf619468a9e9e60c32fd80cb64f43434b8eca
Josia Almeida <71391114+JosiaAlmeida@users.noreply.github.com>
2021-03-12 19:06:07 +0100

    Tela Book

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import React from "react"
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 
 import Login from "./assets/Pages/Login"
+import Book from "./assets/Pages/Book"
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
-                <Route path="/" component={Login}/>
+                <Route path="/" exact component={Login}/>
+                <Route path="/Book" component={Book}/>
             </Switch>
         </BrowserRouter>
     )
